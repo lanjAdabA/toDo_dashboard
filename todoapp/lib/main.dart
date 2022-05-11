7f4055522133877eb98cf7a42cddac434cf7c392
jaeC <thingbaijamjessikhuman@gmail.com>
2022-05-12 01:39:59 +0530

    alignment left

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,9 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
-
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
         title: title,
-        theme: ThemeData(primarySwatch: Colors.amber),
+        theme: ThemeData(primarySwatch: Colors.orange),
         home: const MainPage(title: title),
       );
 }
@@ -24,6 +19,7 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatefulWidget {
   final String title;
 
+  // ignore: use_key_in_widget_constructors
   const MainPage({
     required this.title,
   });
@@ -36,6 +32,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
+          // backgroundColor: Colors.grey,
           title: Text(widget.title),
           leading: const Icon(Icons.menu),
           actions: const [
@@ -53,9 +50,9 @@ class _MainPageState extends State<MainPage> {
               crossAxisCount: 2,
               children: [
                 buildRoundedCard1(),
+                buildRoundedCard2(),
                 buildRoundedCard1(),
-                buildRoundedCard1(),
-                buildRoundedCard1(),
+                buildRoundedCard2(),
               ],
             ),
           ],
@@ -64,23 +61,23 @@ class _MainPageState extends State<MainPage> {
 
   buildQuoteCard() => Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 3,
         child: Padding(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(26),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Align(
-                alignment: Alignment.topRight,
-              ),
               Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const Text(
                     "Most Priority Task",
-                    style: TextStyle(color: Colors.red),
+                    style: TextStyle(color: Colors.red, fontSize: 16),
                   ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       const Icon(
@@ -109,31 +106,25 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ],
               ),
-              Center(
-                child: Column(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        /* Align(
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  /* Align(
                           alignment: Alignment.centerRight,
                         ),
                         */
-                        Icon(
-                          Icons.more_horiz_outlined,
-                          color: Colors.red,
-                          size: 38,
-                        ),
-                        Text(
-                          "Start",
-                          style: TextStyle(color: Colors.green),
-                        ),
-                        Text("2:00:00"),
-                      ],
-                    ),
-                  ],
-                ),
+                  Icon(
+                    Icons.more_horiz_outlined,
+                    color: Colors.red,
+                    size: 30,
+                  ),
+                  Text(
+                    "Start",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                  Text("2:00:00"),
+                ],
               ),
             ],
           ),
@@ -145,27 +136,90 @@ class _MainPageState extends State<MainPage> {
           borderRadius: BorderRadius.circular(12),
         ),
         child: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              Text(
-                'Rounded card',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Icon(
+                      Icons.work_outline_rounded,
+                      size: 32,
+                      color: Colors.brown,
+                    ),
+                    Text(
+                      "5 tasks",
+                      style: TextStyle(color: Colors.black, fontSize: 32),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 12,
-                width: 1,
-              ),
-              Text(
-                'This card',
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Work",
+                        style: TextStyle(color: Colors.black, fontSize: 24)),
+                    const Text("office project work",
+                        style: TextStyle(color: Colors.black, fontSize: 10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text("2-completed",
+                            style:
+                                TextStyle(color: Colors.green, fontSize: 14)),
+                        Text("2-due",
+                            style: TextStyle(color: Colors.red, fontSize: 14)),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      );
+
+  buildRoundedCard2() => Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
         ),
+        child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Icon(
+                      Icons.home_outlined,
+                      size: 32,
+                      color: Colors.brown,
+                    ),
+                    Text(
+                      "5 tasks",
+                      style: TextStyle(color: Colors.black, fontSize: 32),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Work",
+                        style: TextStyle(color: Colors.black, fontSize: 24)),
+                    const Text("office project work",
+                        style: TextStyle(color: Colors.black, fontSize: 10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text("2-completed",
+                            style:
+                                TextStyle(color: Colors.green, fontSize: 14)),
+                        Text("2-due",
+                            style: TextStyle(color: Colors.red, fontSize: 14)),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )),
       );
 }
